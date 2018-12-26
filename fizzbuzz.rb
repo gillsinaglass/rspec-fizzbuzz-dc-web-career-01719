d075439d048bea1faf4e17ee85c0949f0a040e57
Ryan Gill <rdgill95@gmail.com>
2018-12-26 21:56:57 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,7 +1,9 @@
 def fizzbuzz(num)
-  if num % 3 
+  if num % 15 == 0
+    return "FizzBuzz"
+  elsif num % 3 == 0 
     return "Fizz"
-  elsif num == 5
+  elsif num % 5 == 0
     return "Buzz"
   elsif num ==   
     
